f874cbb814582e892f5ae2e1819f60ad5c39adc1
Foo Joon Sunn <joonsunn.foo@gmail.com>
2024-04-10 00:55:36 +0800

    adjusted styling of text input

--- src/components/TextInputDebounced.js
+++ src/components/TextInputDebounced.js
@@ -3,7 +3,9 @@ import { useState } from "react";
 
 function TextInputDebounced({ setText, label, type, ...props }) {
   const [timeoutId, setTimeoutId] = useState(null);
+  const [innerText, setInnerText] = useState("");
   const handleTextInput = (event) => {
+    setInnerText(event.target.value);
     clearTimeout(timeoutId);
     const timeout = setTimeout(() => {
       setText(
@@ -21,7 +23,27 @@ function TextInputDebounced({ setText, label, type, ...props }) {
       onChange={(event) => {
         handleTextInput(event);
       }}
+      value={innerText}
       {...props}
+      sx={{
+        "& label.Mui-focused": {
+          color: "black",
+        },
+        "& .MuiInput-underline:after": {
+          borderBottomColor: "black",
+        },
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": {
+            borderColor: "black",
+          },
+          "&:hover fieldset": {
+            borderColor: "black",
+          },
+          "&.Mui-focused fieldset": {
+            borderColor: "black",
+          },
+        },
+      }}
     />
   );
 }
