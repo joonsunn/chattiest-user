2655c77f0712f75df38b293b1ce55717866214f4
Foo Joon Sunn <joonsunn.foo@gmail.com>
2024-04-10 15:57:16 +0800

    clean up unused files

--- src/components/DialogInfo.js
+++ src/components/DialogInfo.js
@@ -30,16 +30,16 @@ function SimpleDialog(props) {
   };
 
   const text = `<user1> The quick brown fox jumps
-<user2> Why is the fox jumping?
-Did it step on fire?
-<user1> Bruh why you interrupt!
-<user3> I want milk
-<user3> This chat is boring
-<user3> I want roti canai
-<user2> lol you need to chill
-The night is still young why you stress
-<user1> yo I'm just trying to test this
-Please play along!`;
+  <user2> Why is the fox jumping?
+  Did it step on fire?
+  <user1> Bruh why you interrupt!
+  <user3> I want milk
+  <user3> This chat is boring
+  <user3> I want roti canai
+  <user2> lol you need to chill
+  The night is still young why you stress
+  <user1> yo I'm just trying to test this
+  Please play along!`;
 
   const text2 =
     "< > surrounds a username. Words followed by it until the next username will be attributed to that user.";
